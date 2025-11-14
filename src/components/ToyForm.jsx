dc74137f609c9149eb180fb8e43da4ccc95156d2
Joana Corezzi Williams <joanacorezzi@gmail.com>
2025-11-14 15:19:18 -0600

    Step 2: add new toy

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onAddToy }) {
+   const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+ function handleSubmit(event) {
+    event.preventDefault();
+
+    const newToy = {
+      name: name,
+      image: image,
+      likes: 0
+    };
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        onAddToy(data);
+        setName("");
+        setImage("");
+      });
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+           value={name}
+          onChange={(event) => setName(event.target.value)}
         />
         <br />
         <input
@@ -17,6 +46,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={image}
+          onChange={(event) => setImage(event.target.value)}
         />
         <br />
         <input
